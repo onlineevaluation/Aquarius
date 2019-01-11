268d165b8c4802de96eb0825a7958936fe5e816d
youngxhui <youngxhui@gmail.com>
2019-01-11 17:52:25 +0800

    update readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 [![Build Status](https://travis-ci.org/youngxhui/student-end.svg?branch=master)](https://travis-ci.org/youngxhui/student-end)
 
-[](https://student-8ea41.firebaseapp.com/)
+[网站部署地址](https://student-8ea41.firebaseapp.com/)
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,28 +1,24 @@
-import {NgModule, Optional, SkipSelf} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {HeaderComponent} from './header/header.component';
-import {FooterComponent} from './footer/footer.component';
-import {SharedModule} from '../shared/shared.module';
-import {MatIconRegistry} from '@angular/material';
-import {DomSanitizer} from '@angular/platform-browser';
-import {loadSvgResource} from '../utils/svg.util';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SharedModule } from '../shared/shared.module';
+import { MatIconRegistry } from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { loadSvgResource } from '../utils/svg.util';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [
-    SharedModule,
-    BrowserAnimationsModule,
-  ],
-  exports: [
-    HeaderComponent,
-    FooterComponent,
-  ]
+  imports: [SharedModule, BrowserAnimationsModule],
+  exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule {
-  constructor(@Optional() @SkipSelf() parent: CoreModule,
-              ir: MatIconRegistry,
-              ds: DomSanitizer) {
+  constructor(
+    @Optional() @SkipSelf() parent: CoreModule,
+    ir: MatIconRegistry,
+    ds: DomSanitizer,
+  ) {
     if (parent) {
       throw new Error('已经存在core module');
     }
