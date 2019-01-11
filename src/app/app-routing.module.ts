4a3d6bf0122f0fb4df7f21495e923b523ee7003a
youngxhui <youngxhui@gmail.com>
2019-01-11 17:48:25 +0800

    update readme

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,17 @@
-import {NgModule} from '@angular/core';
-import {RouterModule, Routes} from '@angular/router';
-
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {path: '', loadChildren: './login/login.module#LoginModule'},
-  {path: 'exam', loadChildren: './exam/exam.module#ExamModule'},
-  {path: 'page', loadChildren: './page/page.module#PageModule'},
-
+  { path: '', loadChildren: './login/login.module#LoginModule' },
+  { path: 'exam', loadChildren: './exam/exam.module#ExamModule' },
+  { path: 'page', loadChildren: './page/page.module#PageModule' },
 
   // 404 页面
-  {path: '**', loadChildren: './code404/code404.module#Code404Module'},
+  { path: '**', loadChildren: './code404/code404.module#Code404Module' },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule {
-}
+export class AppRoutingModule {}
