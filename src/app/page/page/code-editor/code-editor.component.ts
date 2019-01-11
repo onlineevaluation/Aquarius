3022b5fbd52865f4e0351efd440f138d1564c2a1
youngxhui <youngxhui@gmail.com>
2019-01-11 16:51:39 +0800

    添加ci/cd

--- src/app/page/page/code-editor/code-editor.component.ts
+++ src/app/page/page/code-editor/code-editor.component.ts
@@ -19,7 +19,7 @@ export class CodeEditorComponent implements OnInit {
   public code: any;
   selectedLan = 'c';
   selectedTheme = 'vs';
-  public editorOptions;
+  public editorOptions:any;
 
   constructor() {
 
@@ -29,7 +29,7 @@ export class CodeEditorComponent implements OnInit {
     this.code = '#include <stdio.h>\n\nint main() {\n  // write your code\n  return 0;\n} ';
     this.editorOptions = {theme: this.selectedTheme, language: this.selectedLan};
   }
-
+// 1/V1ixbJDC1fWUKGHDLz0quj9nsx8ovrJEOgre29hpdd4
   /**
    * 改变语言
    * @param language 选择的语言
