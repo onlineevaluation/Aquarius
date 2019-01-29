879499c0f9da8bc763a8e9bcc99c7f01d29ca648
youngxhui <youngxhui@gmail.com>
2019-01-29 16:06:36 +0800

    简化 utils
    去除重复代码

--- src/app/utils/auth.util.ts
+++ src/app/utils/auth.util.ts
@@ -4,20 +4,17 @@ import { JwtUser } from '../domain/JwtUser';
 export const authInfo = () => {
   const jwtHelperService = new JwtHelperService();
   let token = localStorage.getItem('token');
-
+  const jwtUser = new JwtUser();
   if (token !== null) {
     const user = jwtHelperService.decodeToken(token);
-    const jwtUser = new JwtUser();
     jwtUser.auth = user.auth;
     jwtUser.exp = user.exp;
     jwtUser.iat = user.iat;
     jwtUser.sub = user.sub;
     jwtUser.classId = user.classId;
     jwtUser.userId = user.userId;
-    return jwtUser;
   } else {
-    const jwtUser = new JwtUser();
     jwtUser.userId = -1;
-    return jwtUser;
   }
+  return jwtUser;
 };
